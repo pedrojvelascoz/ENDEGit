3f5936f40a5ecd84c05a9fb81771a7cd0d5bea72
alumno <alumno@57016-Win10-Odoo.mshome.net>
2022-08-09 10:13:55 +0200

    subiendo push

--- pruebaPedroGit/src/PacketPrueba/Principal.java
+++ pruebaPedroGit/src/PacketPrueba/Principal.java
@@ -5,7 +5,8 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Probando versiones");
 		System.out.println("Segunda prueba");
-
+		System.out.println("Otra prueba");
+		System.out.println("Commit and push");
 	}
 
 }
