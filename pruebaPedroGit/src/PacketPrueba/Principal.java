7f148f134ec58de779640fef27da771bf5de2929
alumno <alumno@57016-Win10-Odoo.mshome.net>
2022-08-09 10:46:18 +0200

    pushea

--- pruebaPedroGit/src/PacketPrueba/Principal.java
+++ pruebaPedroGit/src/PacketPrueba/Principal.java
@@ -7,7 +7,6 @@ public class Principal {
 		System.out.println("Segunda prueba");
 		System.out.println("Otra prueba");
 		System.out.println("Commit and push!");
-		System.out.println("Commit and push!");
 	}
 
 }
